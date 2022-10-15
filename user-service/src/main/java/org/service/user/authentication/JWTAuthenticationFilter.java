9791b3604b2992b0e7e485f82746967bc3c61c30
maneesh54321 <maneesh54321@gmail.com>
2022-10-16 01:00:02 +0530

    fix cookies null pointer

--- user-service/src/main/java/org/service/user/authentication/JWTAuthenticationFilter.java
+++ user-service/src/main/java/org/service/user/authentication/JWTAuthenticationFilter.java
@@ -36,7 +36,7 @@ public class JWTAuthenticationFilter extends GenericFilterBean {
             chain.doFilter(request, response);
         } else {
             Cookie[] cookies = httpServletRequest.getCookies();
-            if (cookies.length > 0) {
+            if (cookies!= null && cookies.length > 0) {
                 String jwtToken = httpServletRequest.getCookies()[0].getValue();
                 log.info("Token in the request is: {}", jwtToken);
                 if (tokenManager.isTokenValid(jwtToken)) {
