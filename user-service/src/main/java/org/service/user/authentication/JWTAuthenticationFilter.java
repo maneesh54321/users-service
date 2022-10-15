c24e0554c82921ce27f6c5f426aa419fd14b3f5d
maneesh54321 <maneesh54321@gmail.com>
2022-10-15 10:28:47 +0530

    Add authentication!!!

---
+++ user-service/src/main/java/org/service/user/authentication/JWTAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package org.service.user.authentication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.service.user.jwt.TokenManager;
+import org.service.user.vo.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JWTAuthenticationFilter extends GenericFilterBean {
+
+    @Autowired
+    private TokenManager tokenManager;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        String jwtToken = httpServletRequest.getCookies()[0].getValue();
+
+        log.info("Token in the request is: {}", jwtToken);
+
+        if(((HttpServletRequest) request).getRequestURI().matches(".*(swagger|login|signup)+.*") || tokenManager.isTokenValid(jwtToken)) {
+            chain.doFilter(request, response);
+        } else {
+            Response authResponse = new Response("Unauthenticated user", HttpStatus.UNAUTHORIZED);
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.setStatus(401);
+            response.getOutputStream().write(objectMapper.writeValueAsBytes(authResponse));
+        }
+    }
+}
--- user-service/src/main/java/org/service/user/jwt/TokenManager.java
+++ user-service/src/main/java/org/service/user/jwt/TokenManager.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.Base64;
+import java.util.function.Predicate;
 
 @Component
 public class TokenManager {
@@ -19,6 +22,9 @@ public class TokenManager {
     private static final TokenHeader JWT_TOKEN_HEADER = new TokenHeader("SHA512", TokenType.JWT);
 
     private static final String SECRET_SALT = "The secret salt of users-service";
+
+    private static final Predicate<Long> hasExpired =
+            expiryTime -> Instant.ofEpochMilli(expiryTime).isAfter(Instant.now());
 
     @Autowired
     public TokenManager(ObjectMapper objectMapper) {
@@ -60,5 +66,42 @@ public class TokenManager {
 
     private String base64Encode(byte[] input) {
         return new String(Base64.getEncoder().encode(input));
+    }
+
+
+    public boolean isTokenValid(String token) {
+        if (StringUtils.hasText(token)) {
+            String[] tokenParts = token.split("\\.");
+            if (tokenParts.length == 3) {
+                try {
+                    String encodedPayload = tokenParts[1];
+                    TokenPayload tokenPayload = objectMapper.readValue(Base64.getDecoder().decode(encodedPayload), TokenPayload.class);
+                    String encodedHeader = tokenParts[0];
+                    TokenHeader tokenHeader = objectMapper.readValue(
+                            Base64.getDecoder().decode(encodedHeader),
+                            TokenHeader.class
+                    );
+                    return isTokenUnhampered(
+                            encodedHeader,
+                            encodedPayload,
+                            tokenHeader.getAlgorithm(),
+                            tokenParts[2]) &&
+                            hasExpired.test(tokenPayload.getExpiryTime()
+                            );
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isTokenUnhampered(String encodedHeader, String encodedPayload, String algorithm, String signature) {
+        String calculatedSignature = createSignature(
+                encodedHeader,
+                encodedPayload,
+                algorithm
+        );
+        return calculatedSignature.equals(signature);
     }
 }
